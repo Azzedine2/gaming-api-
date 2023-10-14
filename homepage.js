5d7755314199e22715972c933be58cefa341d191
Azzedine2 <136700190+Azzedine2@users.noreply.github.com>
2023-10-14 08:46:30 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,6 +2,13 @@
 const key = "a3ed24058c6960422612f376ca988d6bdc6d4a67";
 
 // URL GiantBomb API
-const url = "";
+const url = `https://www.giantbomb.com/api/game/3030-63/?api_key=${key}
+&format=json`;
+// const url = `https://www.giantbomb.com/api/games/?api_key=${key}&format=json`;
+// 3030-63
 
 //  Fetch
+fetch(url)
+  .then((res) => res.json())
+  .then((json) => console.log(json.results))
+  .catch((err) => console.log(err));
