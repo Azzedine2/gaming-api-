0bdb58f27f28cd8949a5de36a8e64faeb4905728
Azzedine2 <136700190+Azzedine2@users.noreply.github.com>
2023-10-22 18:25:47 +0200

    Update searchFunction.js

--- js/searchFunction.js
+++ js/searchFunction.js
@@ -4,11 +4,23 @@ import { recommendation } from "./recommendations.js";
 
 console.log(fetchGameSearch);
 
+// API Key
+const key = "a3ed24058c6960422612f376ca988d6bdc6d4a67";
+
+// DOM Elements
+const searchTerm = document.querySelector("#search").value;
+
 async function searchGame() {
   try {
     // Function Call
-    fetchGameSearch();
-    recommendation();
+    // fetchGameSearch();
+    // recommendation();
+
+    // New test
+    // Call by Searches Needs
+
+    // Search Page Api Call
+    const urlGameBySearch = `https://www.giantbomb.com/api/search/?api_key=${key}&format=json&query=${searchTerm}`;
 
     // Confirmation / Checker
     console.log("Load Game success");
