a11a718a145d6219534919a75aaf98e70d9926c4
Azzedine2 <136700190+Azzedine2@users.noreply.github.com>
2023-10-27 11:01:35 +0200

    Update dummy.js

--- About page section/DummySection/dummy.js
+++ About page section/DummySection/dummy.js
@@ -5,7 +5,7 @@ const guid = "3030-11237";
 
 // const urlVideo = `https://www.giantbomb.com/api/video/${guid}/?api_key=${key}&format=json`;
 const urlVideo = `https://www.giantbomb.com/api/game/${guid}/?api_key=${key}&format=json`;
-bgVideoDom.src = "https://www.youtube.com/watch?v=46BzOBWQXP8&ab_channel=AlexPlayz";
+bgVideoDom.src = "https://www.youtube.com/embed/46BzOBWQXP8";
 console.log("urlVideo: ", urlVideo);
 
 // try {
@@ -18,3 +18,5 @@ console.log("urlVideo: ", urlVideo);
 // } catch (error) {
 //   console.log("error from dummy call: ", error);
 // }
+
+{/* <iframe width="1696" height="838" src="https://www.youtube.com/embed/46BzOBWQXP8" title="75 Minutes of Relaxing and Calming Nintendo Music" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
